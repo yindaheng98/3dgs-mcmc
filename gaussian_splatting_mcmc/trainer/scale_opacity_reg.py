49c938e02d90b47998dd28b3ff8822b97157e752
yindaheng98 <yindaheng98@gmail.com>
2025-06-29 16:39:57 -0700

    ScaleOpacityRegularizer

--- gaussian_splatting_mcmc/trainer/scale_opacity_reg.py
+++ gaussian_splatting_mcmc/trainer/scale_opacity_reg.py
@@ -5,41 +5,50 @@ from gaussian_splatting import Camera, GaussianModel
 from gaussian_splatting.trainer import AbstractTrainer, TrainerWrapper, BaseTrainer
 
 
-class OpacityRegularizer(TrainerWrapper):
+class ScaleOpacityRegularizer(TrainerWrapper):
     def __init__(
             self, base_trainer: AbstractTrainer,
+            scale_reg_from_iter=0,
+            scale_reg_weight=0.01,
             opacity_reg_from_iter=0,
             opacity_reg_weight=0.01,
     ):
         super().__init__(base_trainer)
+        self.scale_reg_from_iter = scale_reg_from_iter
+        self.scale_reg_weight = scale_reg_weight
         self.opacity_reg_from_iter = opacity_reg_from_iter
         self.opacity_reg_weight = opacity_reg_weight
 
     def loss(self, out: dict, camera: Camera) -> torch.Tensor:
         if self.curr_step < self.opacity_reg_from_iter:
             return super().loss(out, camera)
-        return super().loss(out, camera) + self.opacity_reg_weight * torch.abs(self.model.get_opacity).mean()
+        loss = super().loss(out, camera)
+        loss += self.scale_reg_weight * torch.abs(self.model.get_scaling).mean()
+        loss += self.opacity_reg_weight * torch.abs(self.model.get_opacity).mean()
+        return loss
 
 
-def OpacityRegularizeTrainerWrapper(
+def ScaleOpacityRegularizeTrainerWrapper(
     base_constructor,
     model: GaussianModel,
-    scene_extent: float,
     *args,
+    scale_reg_from_iter=0,
+    scale_reg_weight=0.01,
     opacity_reg_from_iter=0,
     opacity_reg_weight=0.01,
     **kwargs
-) -> OpacityRegularizer:
-    return OpacityRegularizer(
+) -> ScaleOpacityRegularizer:
+    return ScaleOpacityRegularizer(
         base_constructor(
             model,
-            scene_extent,
             *args, **kwargs
         ),
+        scale_reg_from_iter=scale_reg_from_iter,
+        scale_reg_weight=scale_reg_weight,
         opacity_reg_from_iter=opacity_reg_from_iter,
         opacity_reg_weight=opacity_reg_weight,
     )
 
 
-def BaseOpacityRegularizeTrainer(model: GaussianModel, scene_extent: float, *args, **kwargs) -> OpacityRegularizer:
-    return OpacityRegularizeTrainerWrapper(BaseTrainer, model, scene_extent, *args, **kwargs)
+def BaseScaleOpacityRegularizeTrainer(model: GaussianModel, scene_extent: float, *args, **kwargs) -> ScaleOpacityRegularizer:
+    return ScaleOpacityRegularizeTrainerWrapper(BaseTrainer, model, scene_extent, *args, **kwargs)
