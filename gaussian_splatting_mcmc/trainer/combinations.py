a5aba06438dc85c829135cb9c9a3ea685647a3c2
yindaheng98 <yindaheng98@gmail.com>
2025-06-29 22:01:22 -0700

    ScaleOpacityRegularizeNoiseRelocationDensifierTrainerWrapper

--- gaussian_splatting_mcmc/trainer/combinations.py
+++ gaussian_splatting_mcmc/trainer/combinations.py
@@ -11,13 +11,38 @@ def NoiseRelocationDensifierTrainerWrapper(
         noargs_base_densifier_constructor: Callable[[GaussianModel, float], AbstractDensifier],
         model: GaussianModel,
         scene_extent: float,
-        *args, **kwargs):
+        *args,
+        noise_lr=5e5,
+        **kwargs):
     return NoiseWrapper(
         lambda model, scene_extent, *args, **kwargs: Relocater(model, scene_extent, noargs_base_densifier_constructor(model, scene_extent), *args, **kwargs),
-        model, scene_extent, *args, **kwargs)
+        model, scene_extent,
+        *args,
+        noise_lr=noise_lr,
+        **kwargs)
 
 
-MCMCDensifierTrainerWrapper = NoiseRelocationDensifierTrainerWrapper
+def ScaleOpacityRegularizeNoiseRelocationDensifierTrainerWrapper(
+        noargs_base_densifier_constructor: Callable[[GaussianModel, float], AbstractDensifier],
+        model: GaussianModel,
+        *args,
+        scale_reg_from_iter=0,
+        scale_reg_weight=0.01,
+        opacity_reg_from_iter=0,
+        opacity_reg_weight=0.01,
+        **kwargs):
+    return ScaleOpacityRegularizeTrainerWrapper(
+        NoiseRelocationDensifierTrainerWrapper,
+        noargs_base_densifier_constructor, model,
+        *args,
+        scale_reg_from_iter=scale_reg_from_iter,
+        scale_reg_weight=scale_reg_weight,
+        opacity_reg_from_iter=opacity_reg_from_iter,
+        opacity_reg_weight=opacity_reg_weight,
+        **kwargs)
+
+
+MCMCDensifierTrainerWrapper = ScaleOpacityRegularizeNoiseRelocationDensifierTrainerWrapper
 
 
 def NoiseRelocationTrainer(model: GaussianModel, scene_extent: float, *args, **kwargs):
