137c9fbc281d7419267df64b8a142da8d39e26dc
yindaheng98 <yindaheng98@gmail.com>
2025-11-20 22:03:10 -0800

    correct format of reg

--- gaussian_splatting_mcmc/trainer/scale_opacity_reg.py
+++ gaussian_splatting_mcmc/trainer/scale_opacity_reg.py
@@ -1,4 +1,5 @@
 
+from typing import Callable
 import torch
 
 from gaussian_splatting import Camera, GaussianModel
@@ -29,8 +30,9 @@ class ScaleOpacityRegularizer(TrainerWrapper):
 
 
 def ScaleOpacityRegularizeTrainerWrapper(
-    base_constructor,
+    base_trainer_constructor: Callable[..., AbstractTrainer],
     model: GaussianModel,
+    scene_extent: float,
     *args,
     scale_reg_from_iter=0,
     scale_reg_weight=0.01,
@@ -39,10 +41,7 @@ def ScaleOpacityRegularizeTrainerWrapper(
     **kwargs
 ) -> ScaleOpacityRegularizer:
     return ScaleOpacityRegularizer(
-        base_constructor(
-            model,
-            *args, **kwargs
-        ),
+        base_trainer_constructor(model, scene_extent, *args, **kwargs),
         scale_reg_from_iter=scale_reg_from_iter,
         scale_reg_weight=scale_reg_weight,
         opacity_reg_from_iter=opacity_reg_from_iter,
@@ -52,3 +51,5 @@ def ScaleOpacityRegularizeTrainerWrapper(
 
 def BaseScaleOpacityRegularizeTrainer(model: GaussianModel, scene_extent: float, *args, **kwargs) -> ScaleOpacityRegularizer:
     return ScaleOpacityRegularizeTrainerWrapper(BaseTrainer, model, scene_extent, *args, **kwargs)
+
+# similar to gaussian_splatting.trainer.depth
