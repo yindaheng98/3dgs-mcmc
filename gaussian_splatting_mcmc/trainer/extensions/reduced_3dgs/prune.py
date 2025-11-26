1202ad86b7e93335250cefd4f648dad3af6243ba
yindaheng98 <yindaheng98@gmail.com>
2025-11-25 19:52:09 -0800

    MCMCImportancePruningTrainer by replace FullPruning as ImportancePruning

--- gaussian_splatting_mcmc/trainer/extensions/reduced_3dgs/prune.py
+++ gaussian_splatting_mcmc/trainer/extensions/reduced_3dgs/prune.py
@@ -2,7 +2,7 @@ from functools import partial
 from typing import Callable
 from gaussian_splatting import GaussianModel
 from gaussian_splatting.dataset import CameraDataset, TrainableCameraDataset
-from gaussian_splatting.trainer import DepthTrainerWrapper, OpacityResetTrainerWrapper
+from gaussian_splatting.trainer import DepthTrainerWrapper
 from gaussian_splatting.trainer.densifier import AbstractDensifier, NoopDensifier
 from gaussian_splatting_mcmc.trainer import MCMCTrainerWrapper
 from reduced_3dgs.shculling import VariableSHGaussianModel, SHCullingTrainerWrapper
