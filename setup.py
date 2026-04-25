77bbfd2cfec400d0c293042b364d697af4c0e48b
yindaheng98 <yindaheng98@gmail.com>
2026-04-24 23:35:39 -0700

    upgrade

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if os.name == 'nt':
 
 setup(
     name="gaussian_splatting_mcmc",
-    version='1.3.0',
+    version='1.3.1',
     author='yindaheng98',
     author_email='yindaheng98@gmail.com',
     url='https://github.com/yindaheng98/3dgs-mcmc',
@@ -62,7 +62,7 @@ setup(
         'build_ext': BuildExtension
     },
     install_requires=[
-        'gaussian-splatting >= 2.3.0',
+        'gaussian-splatting >= 2.3.8',
         'reduced-3dgs',
     ]
 )
